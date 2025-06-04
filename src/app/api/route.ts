afc685f3f99a0b6fab08485e86c0d374b1f83dda
hyperspacehq <andrewt@hyperspacehq.com>
2025-06-04 16:47:13 +0700

    authenapp

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -2,15 +2,11 @@ import { NextResponse } from 'next/server'
 
 export async function GET() {
   return NextResponse.json({
-    title: "New CRM Card",
-    fetch: {
-      targetUrl: "https://googledrivehubspot.vercel.app/api",
-      objectTypes: [
-        {
-          name: "contacts",
-          propertiesToSend: ["firstname", "email", "lastname"]
-        }
-      ]
-    }
+    type: "IFRAME",
+    width: 890,
+    height: 748,
+    uri: "https://example.com/iframe-contents",
+    label: "Edit",
+    associatedObjectProperties: ["some_crm_property"]
   })
 }
