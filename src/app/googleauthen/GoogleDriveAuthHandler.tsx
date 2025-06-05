92eb4312ea241d2f7f296fe83ce265eae2d023b1
hyperspacehq <andrewt@hyperspacehq.com>
2025-06-05 11:32:39 +0700

    authen google

--- src/app/googleauthen/GoogleDriveAuthHandler.tsx
+++ src/app/googleauthen/GoogleDriveAuthHandler.tsx
@@ -12,6 +12,7 @@ export default function GoogleDriveAuthHandler() {
 
       if (!code) {
         const clientId = process.env.GOOGLE_CLIENT_ID!;
+        console.log('clientId', clientId);
         const redirectUri = encodeURIComponent('https://googledrivehubspot.vercel.app/googleauthen');
         const scope = encodeURIComponent('https://www.googleapis.com/auth/drive.file');
         const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&access_type=offline&prompt=consent`;
